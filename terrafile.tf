4741e1c04e2cd2267847a55dca9a2c2afb1f621b
Rafael Gomes <rgomes@dnsfilter.com>
2023-07-08 13:16:10 -0300

    feat: aplicar nova versão
    Liberar a porta do grafana

--- terrafile.tf
+++ terrafile.tf
@@ -1,4 +1,11 @@
 module "produto" {
-  source        = "git@github.com:gomex/iac_module_server.git?ref=v1.0.0"
+  source        = "git@github.com:gomex/iac_module_server.git?ref=v1.2.1"
   instance_type = "t3.micro"
+  sg_group_rule = {
+    grafana = {
+      cidr_blocks = ["0.0.0.0/0"]
+      port        = "3000"
+      protocol    = "tcp"
+    },
+  }
 }
