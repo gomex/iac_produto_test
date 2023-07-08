63f4bafafd9bc3b664026597701eabacd8359e54
Rafael Gomes <rgomes@dnsfilter.com>
2023-07-08 10:08:02 -0300

    feat: Aplicar versão e formatação

--- terrafile.tf
+++ terrafile.tf
@@ -1,4 +1,4 @@
 module "produto" {
-  source = "git@github.com:gomex/iac_module_server.git?ref=main"
-  instance_type            = "t3.micro"
+  source        = "git@github.com:gomex/iac_module_server.git?ref=v1.0.0"
+  instance_type = "t3.micro"
 }
